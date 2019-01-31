273407b2760eeb1f774809c2c7570df352699309
carlosclabsis <carlosclabsis@gmail.com>
2019-01-31 10:49:31 -0500

    Modificacion del Screen Reserva
    -Añadi formulario para realizar los pagos mediante tarjetas de credito.

--- canchita/src/app/components/grilla/grilla.component.ts
+++ canchita/src/app/components/grilla/grilla.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterContentInit } from '@angular/core';
 
 @Component({
   selector: 'app-grilla',
   templateUrl: './grilla.component.html',
   styleUrls: ['./grilla.component.css']
 })
-export class GrillaComponent implements OnInit {
+export class GrillaComponent implements OnInit, AfterContentInit{
 
   constructor() { }
 
   ngOnInit() {
   }
+  ngAfterContentInit(){
+    
+  }
+
+  
+  
 
 }
